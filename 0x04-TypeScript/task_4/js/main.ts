59a8f994ab83a9dd84cb543aa06b370f4dad17aa
garisonmike <garisonmike006@gmail.com>
2025-11-10 00:02:04 +0300

    task errors fix

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -4,27 +4,29 @@
 /// <reference path="subjects/Java.ts" />
 /// <reference path="subjects/React.ts" />
 
-const cpp = new Subjects.Cpp();
-const java = new Subjects.Java();
-const react = new Subjects.React();
+namespace Subjects {
+    export const cpp = new Cpp();
+    export const java = new Java();
+    export const react = new React();
 
-const cTeacher: Subjects.Teacher = {
-    firstName: "John",
-    lastName: "Doe",
-    experienceTeachingC: 10
-};
+    export const cTeacher: Teacher = {
+        firstName: "John",
+        lastName: "Doe",
+        experienceTeachingC: 10
+    };
 
-console.log("C++");
-cpp.setTeacher(cTeacher);
-console.log(cpp.getRequirements());
-console.log(cpp.getAvailableTeacher());
+    console.log("C++");
+    cpp.setTeacher(cTeacher);
+    console.log(cpp.getRequirements());
+    console.log(cpp.getAvailableTeacher());
 
-console.log("Java");
-java.setTeacher(cTeacher);
-console.log(java.getRequirements());
-console.log(java.getAvailableTeacher());
+    console.log("Java");
+    java.setTeacher(cTeacher);
+    console.log(java.getRequirements());
+    console.log(java.getAvailableTeacher());
 
-console.log("React");
-react.setTeacher(cTeacher);
-console.log(react.getRequirements());
-console.log(react.getAvailableTeacher());
+    console.log("React");
+    react.setTeacher(cTeacher);
+    console.log(react.getRequirements());
+    console.log(react.getAvailableTeacher());
+}
